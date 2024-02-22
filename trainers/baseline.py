3c91da40d38bcd20b1dedf7bce7e556e71ddbd51
ManhNV0907 <manhqh0909@gmail.com>
2024-02-22 07:53:50 +0000

    [Update]

--- trainers/baseline.py
+++ trainers/baseline.py
@@ -12,6 +12,8 @@ from utils.read_data import statistic
 import torch.nn.functional as F
 from sklearn.mixture import GaussianMixture
 import random
+from scipy.optimize import minimize
+
 
 class Trainer:
 
@@ -282,7 +284,7 @@ class Trainer:
                     # shared_grad = AUGD(torch.stack([distill_shared_grad, loss_shared_grad, loss_mem_shared_grad, distill_mem_shared_grad]))["updating_grad"]
                     # mtl_output = AUGD(torch.stack([distill_shared_grad, loss_shared_grad, loss_mem_shared_grad, distill_mem_shared_grad]))
                     # mtl_output = AUGD(torch.stack([distill_shared_grad, distill_mem_shared_grad]))
-                    mtl_output = PCGrad(torch.stack([distill_shared_grad, loss_shared_grad, loss_mem_shared_grad, distill_mem_shared_grad]))
+                    mtl_output = CAGrad(torch.stack([distill_shared_grad, loss_shared_grad, loss_mem_shared_grad, distill_mem_shared_grad]))
 
 
                     # mtl_output = AUGD(torch.stack([loss_shared_grad, loss_mem_shared_grad]))
@@ -489,3 +491,37 @@ def PCGrad(grads: List[Tuple[torch.Tensor]], reduction: str = "sum") -> torch.Te
         merged_grad = [g / len(grads) for g in merged_grad]
 
     return dict(updating_grad = merged_grad)
+def CAGrad(grads, alpha=0.5, rescale=1):
+    n_tasks = len(grads)
+    grads = grads.t()
+
+    GG = grads.t().mm(grads).cpu()  # [num_tasks, num_tasks]
+    g0_norm = (GG.mean() + 1e-8).sqrt()  # norm of the average gradient
+
+    x_start = np.ones(n_tasks) / n_tasks
+    bnds = tuple((0, 1) for x in x_start)
+    cons = {"type": "eq", "fun": lambda x: 1 - sum(x)}
+    A = GG.numpy()
+    b = x_start.copy()
+    c = (alpha * g0_norm + 1e-8).item()
+
+    def objfn(x):
+        return (
+            x.reshape(1, n_tasks).dot(A).dot(b.reshape(n_tasks, 1))
+            + c
+            * np.sqrt(x.reshape(1, n_tasks).dot(A).dot(x.reshape(n_tasks, 1)) + 1e-8)
+        ).sum()
+
+    res = minimize(objfn, x_start, bounds=bnds, constraints=cons)
+    w_cpu = res.x
+    ww = torch.Tensor(w_cpu).to(grads.device)
+    gw = (grads * ww.view(1, -1)).sum(1)
+    gw_norm = gw.norm()
+    lmbda = c / (gw_norm + 1e-8)
+    g = grads.mean(1) + lmbda * gw
+    if rescale == 0:
+        return dict(updating_grad = g)
+    elif rescale == 1:
+        return dict(updating_grad = g / (1 + alpha ** 2))
+    else:
+        return dict(updating_grad = g / (1 + alpha))
