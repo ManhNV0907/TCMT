9c6001bf6049d545abbf7839252ac65959cdf8cd
ManhNV0907 <manhqh0909@gmail.com>
2024-02-24 20:42:23 +0000

    [Update]

--- trainers/baseline.py
+++ trainers/baseline.py
@@ -88,7 +88,7 @@ class Trainer:
             self.key_mixture[label].weights_[0] = 1.0
         #Sample prelogits for each label
         for i, label in enumerate(self.curr_label_set):
-            replay_embedding =  self.key_mixture[label].sample(51200)[0].astype("float32")
+            replay_embedding =  self.key_mixture[label].sample(5120)[0].astype("float32")
             self.buffer_embedding[label].append(torch.tensor(replay_embedding))
         
         if self.task_num ==1:
