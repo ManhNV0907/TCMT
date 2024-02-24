bb37364928948a331e0d3b0fa4b18d2f115e0efb
ManhNV0907 <manhqh0909@gmail.com>
2024-02-24 15:35:30 +0000

    [Update]

---
+++ utils/replay_dataloader.py
@@ -0,0 +1,29 @@
+import random
+import torch
+from torch.utils.data import Dataset, DataLoader
+
+class MemoryDataset(Dataset):
+    def __init__(self, memory, labels):
+        self.memory = memory
+        self.labels = list(labels)
+        self.inputs = []
+        self.labels_list = []
+
+        for label in self.labels:
+            input_ids_list = self.memory.get(label, [])[0]
+            self.inputs.extend(input_ids_list)
+            self.labels_list.extend([label] * len(input_ids_list))
+
+    def __len__(self):
+        return len(self.inputs)
+
+    def __getitem__(self, index):
+        input_ids = self.inputs[index]
+        label = self.labels_list[index]
+        return input_ids, label
+
+def MemoryLoader(memory, batch_size, labels):
+    dataset = MemoryDataset(memory, labels)
+    dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
+    inputs_batch, labels_batch = next(iter(dataloader))
+    return inputs_batch, labels_batch
