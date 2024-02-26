1b28958994bb0475bc21cf537b3849673ada458c
ManhNV0907 <manhqh0909@gmail.com>
2024-02-26 08:30:05 +0000

    [Update]

--- model/baseline.py
+++ model/baseline.py
@@ -13,6 +13,7 @@ class Encoder(nn.Module):
         super().__init__()
         self.args = args
         self.config = AutoConfig.from_pretrained(args.model_name_or_path)
+        self.config.hidden_size = 384
         self.model = AutoModel.from_pretrained(
             args.model_name_or_path, config=self.config)
 
@@ -55,7 +56,7 @@ class Encoder(nn.Module):
         last_hidden_state = outputs.last_hidden_state
         prelogits = mean_pooling(last_hidden_state, attention_mask)
 
-        prelogits = self.dropout(prelogits)
+        # prelogits = self.dropout(prelogits)
         return outputs, prelogits
 
     def forward(
@@ -138,7 +139,7 @@ class Classifier(nn.Module):
         self.num_labels = 0
         self.num_tasks = 0
         self.head = nn.Sequential(
-            nn.Linear(768, 768, bias=True),
+            nn.Linear(768, 384, bias=True),
             nn.ReLU(inplace=True),  
         )
     # def get_cur_classifer(self):
