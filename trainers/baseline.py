d40b91621fa9cec38d05fba636bf464a3440261e
ManhNV0907 <manhqh0909@gmail.com>
2024-02-26 09:50:38 +0000

    [Update]

--- trainers/baseline.py
+++ trainers/baseline.py
@@ -267,8 +267,8 @@ class Trainer:
                     #     ].reshape(param.shape)
                         # total_length += length
 
-                    # training_loss = 5*loss + 5*distill_loss + loss_mem + 10*distill_loss_mem
-                    training_loss = 0.7*loss + 0.3*loss_mem 
+                    training_loss = loss + distill_loss + 2*loss_mem + 2*distill_loss_mem
+                    # training_loss = 0.7*loss + 0.3*loss_mem 
 
                     training_loss.backward()
                     optimizer.step()
