4bb606a2aa4e8445121b089bb66d2c398170e77a
ManhNV0907 <manhqh0909@gmail.com>
2024-03-03 13:05:39 +0000

    Update

--- model/baseline.py
+++ model/baseline.py
@@ -141,10 +141,6 @@ class Classifier(nn.Module):
         self.head = nn.Sequential(
             nn.Linear(768, 768, bias=True),
             nn.ReLU(inplace=True),  
-            nn.Linear(768, 768, bias=True),
-            nn.ReLU(inplace=True),  
-            nn.Linear(768, 768, bias=True),
-            nn.ReLU(inplace=True),  
         )
     # def get_cur_classifer(self):
     #     self.cur_model = None
@@ -152,7 +148,7 @@ class Classifier(nn.Module):
     #     return self.cur_model
 
     def forward(self, x: torch.Tensor):
-        x = self.head(x)
+        # x = self.head(x)
         out = self.top_linear(x)
         return out    
 
